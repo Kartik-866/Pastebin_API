f8d223107e124f620293682fa1f91161bdda4f47
Kartik Mehra <36447692+Kartik-866@users.noreply.github.com>
2019-03-07 19:14:29 +0530

    Update paste.py

--- paste.py
+++ paste.py
@@ -3,7 +3,7 @@ from getpass import getpasss
 
 def pasteAsGuest():
     paste_url = 'https://pastebin.com/api/api_post.php'
-    user_data = {'api_dev_key': api_dev_key,
+    user_data = {'api_dev_key': dev_key,
                 'api_option': 'paste',
                 'api_paste_code': paste_code,
                 'api_paste_name': paste_name
@@ -13,7 +13,7 @@ def pasteAsGuest():
 
 def pasteAsUser():
     paste_url = 'https://pastebin.com/api/api_post.php'
-    user_data = {'api_dev_key': api_dev_key,
+    user_data = {'api_dev_key': dev_key,
                 'api_user_key': temp.text,
                 'api_option': 'paste',
                 'api_paste_code': paste_code,
@@ -24,7 +24,7 @@ def pasteAsUser():
 
 
 
-api_dev_key = ''  #paste your unique API developer key you get after signing in to https://pastebin.com
+dev_key = ''  #paste your unique API developer key you get after signing in to https://pastebin.com
 paste_code = input('What do you want to paste? ')
 paste_name = input('Paste Name: ')
 if not paste_name:
@@ -33,7 +33,7 @@ user_name = input('User Name: ')
 user_password = getpass()
 
 login_url = 'https://pastebin.com/api/api_login.php'
-create_user_key = {'api_dev_key': api_dev_key,
+create_user_key = {'api_dev_key': dev_key,
                     'api_user_name': user_name,
                     'api_user_password': user_password
                     }
